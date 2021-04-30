991c76a6148fd99df80dcf295217b319cc318ec3
saucettv <81865589+saucettv@users.noreply.github.com>
2021-04-30 16:39:33 +0100

    Add files via upload

---
+++ background.js
@@ -0,0 +1,20 @@
+function blockURL(requestDetails) {
+  return {
+    cancel: true
+  };
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+  blockURL,
+  {urls: ["https://*.amazon-adsystem.com/*","https://d2v02itv0y9u9t.cloudfront.net/dist/1.0.5/v6s.js","https://*.imrworldwide.com/*","https://countess.twitch.tv/*","https://*.scorecardresearch.com/*","https://www.googletagservices.com/tag/js/gpt.js","*://*.branch.io/*","*://comscore.com/*"]},
+  ["blocking"]
+);
+
+
+
+
+
+
+
+
+
