64f469edc2660413af4e1e7af06a40ced22ce4f0
saucettv <81865589+saucettv@users.noreply.github.com>
2021-05-08 14:47:08 +0100

    Update background.js

--- background.js
+++ background.js
@@ -6,15 +6,6 @@ function blockURL(requestDetails) {
 
 chrome.webRequest.onBeforeRequest.addListener(
   blockURL,
-  {urls: ["https://*.amazon-adsystem.com/*","https://d2v02itv0y9u9t.cloudfront.net/dist/1.0.5/v6s.js","https://*.imrworldwide.com/*","https://countess.twitch.tv/*","https://*.scorecardresearch.com/*","https://www.googletagservices.com/tag/js/gpt.js","*://*.branch.io/*","*://comscore.com/*"]},
+  {urls: ["https://*.amazon-adsystem.com/*","https://ddacn6pr5v0tl.cloudfront.net/*","https://d2v02itv0y9u9t.cloudfront.net/dist/1.0.5/v6s.js","https://*.imrworldwide.com/*","https://countess.twitch.tv/*","https://*.scorecardresearch.com/*","https://www.googletagservices.com/tag/js/gpt.js","*://*.branch.io/*","*://comscore.com/*"]},
   ["blocking"]
 );
-
-
-
-
-
-
-
-
-
